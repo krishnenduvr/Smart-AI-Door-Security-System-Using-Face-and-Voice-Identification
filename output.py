35c3dfb361f35efa82a6e113925e0d2b892a5a31
krishnenduvr <krishnenduvr1@gmail.com>
2026-01-29 14:59:37 +0530

    Refactor output.py to improve structure and readability

--- output.py
+++ output.py
@@ -27,18 +27,13 @@ def download_face_db(url):
     z = zipfile.ZipFile(io.BytesIO(r.content))
     z.extractall(FACE_DB)
 download_face_db("https://drive.google.com/uc?id=1eehbJs4Z4xAc0PPRV3ANlUQz_KMuoAPG&export=download")
+
+
 # ---------------- PATHS ----------------
-# FACE_DB = r"D:\Security System\cropped_captured"
-VOICE_MODEL_PATH = "voice_model.pkl"
-VOICE_LABEL_PATH = "voice_labels.pkl"
-
-# VOICE_MODEL_PATH = r"D:\Security System\voice_model.pkl"
-# VOICE_LABEL_PATH = r"D:\Security System\voice_labels.pkl"
-
-
+VOICE_MODEL_PATH = r"D:\Security System\voice_model.pkl"
+VOICE_LABEL_PATH = r"D:\Security System\voice_labels.pkl"
 
 TEMP_AUDIO = "temp_voice.wav"
-
 VOICE_DURATION = 4
 VOICE_FS = 22050
 VOICE_THRESHOLD = 0.7
@@ -433,4 +428,5 @@ st.markdown("""
     © 2026 Smart AI Door Security System | All Rights Reserved
 </div>
 """, unsafe_allow_html=True)
+
 
